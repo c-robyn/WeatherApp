a9b2a82d2e74806ad1f72aeea956f3c8eb069f7e
c-robyn <robyncook13@gmail.com>
2023-06-11 17:57:19 -0400

    forecast update

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "3980a7c8f2a782241a093131b099f993";
+  let apiKey = "0694dc9ddf30c85dedc282e5ce86e825";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
